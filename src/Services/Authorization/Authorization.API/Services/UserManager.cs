6a3d72977866ba62622b293c19a75fb4f2441214
Adrian Frankowski <adrian.frankowski95@gmail.com>
2022-05-13 01:49:49 +0200

    Role equals overload and custom user manager

---
+++ src/Services/Authorization/Authorization.API/Services/UserManager.cs
@@ -0,0 +1,28 @@
+using Blog.Services.Authorization.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+
+namespace Blog.Services.Authorization.API.Services;
+
+public class UserManager : UserManager<User>
+{
+    public UserManager(IUserStore<User> store,
+        IOptions<IdentityOptions> optionsAccessor,
+        IPasswordHasher<User> passwordHasher,
+        IEnumerable<IUserValidator<User>> userValidators,
+        IEnumerable<IPasswordValidator<User>> passwordValidators,
+        ILookupNormalizer keyNormalizer,
+        IdentityErrorDescriber errors,
+        IServiceProvider services,
+        ILogger<UserManager<User>> logger)
+        : base(store,
+            optionsAccessor,
+            passwordHasher,
+            userValidators,
+            passwordValidators,
+            keyNormalizer,
+            errors,
+            services,
+            logger)
+    { }
+}
