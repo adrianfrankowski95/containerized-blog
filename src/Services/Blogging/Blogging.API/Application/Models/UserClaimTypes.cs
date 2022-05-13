5e2a2a958d4ad87dd8b71d63b2725bcb6d0f6573
Adrian Frankowski <adrian.frankowski95@gmail.com>
2022-05-13 02:00:26 +0200

    Updated id claim

--- src/Services/Blogging/Blogging.API/Application/Models/UserClaimTypes.cs
+++ src/Services/Blogging/Blogging.API/Application/Models/UserClaimTypes.cs
@@ -2,7 +2,7 @@ namespace Blog.Services.Blogging.API.Application.Models;
 
 public static class UserClaimTypes
 {
-    public const string Id = "id";
+    public const string Id = "sub";
     public const string Role = "role";
     public const string Email = "email";
     public const string Name = "name";
