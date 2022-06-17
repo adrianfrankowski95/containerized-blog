386ca7fafdd34a746610450ad2f990bc0ccb239c
Adrian Frankowski <adrian.frankowski95@gmail.com>
2022-06-17 17:26:40 +0200

    Added email sending, injected options

--- src/Services/Emailing/Emailing.API/Grpc/EmailingService.cs
+++ src/Services/Emailing/Emailing.API/Grpc/EmailingService.cs
@@ -1,24 +1,50 @@
-using FluentEmail.Core.Interfaces;
+using Blog.Services.Emailing.API.Templates.Identity;
+using FluentEmail.Core;
 using Grpc.Core;
+using NodaTime.Serialization.Protobuf;
 
 namespace Blog.Services.Emailing.API.Grpc;
 
 public class EmailingService : GrpcEmailingService.GrpcEmailingServiceBase
 {
     private readonly ILogger<EmailingService> _logger;
-    private readonly ISender _sender;
+    private readonly IFluentEmail _sender;
+
+    private readonly static SendEmailResponse _success = new() { Success = true };
+    private readonly static SendEmailResponse _fail = new() { Success = false };
 
     public EmailingService(
-        ISender sender,
+        IFluentEmail sender,
         ILogger<EmailingService> logger)
     {
         _sender = sender ?? throw new ArgumentNullException(nameof(sender));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    public override Task<SendEmailResponse> SendEmailConfirmationEmail(SendEmailConfirmationEmailRequest request, ServerCallContext context)
+    public override async Task<SendEmailResponse> SendEmailConfirmationEmail(SendEmailConfirmationEmailRequest request, ServerCallContext context)
     {
-        return base.SendEmailConfirmationEmail(request, context);
+        try
+        {
+            var result = await _sender
+                    .To(request.Recipient.EmailAddress, request.Recipient.Name)
+                    .Subject("Email confirmation")
+                    .UsingTemplateFromFile("/Identity/EmailConfirmation",
+                        new EmailConfirmationModel(
+                            request.Recipient.Name,
+                            request.CallbackUrl,
+                            request.UrlExpirationAt.ToInstant()))
+                    .SendAsync().ConfigureAwait(false);
+
+            if (result.Successful)
+                return _success;
+
+            return _fail;
+        }
+        catch (Exception ex)
+        {
+
+            return _fail;
+        }
     }
 
     public override Task<SendEmailResponse> SendPasswordResetEmail(SendPasswordResetEmailRequest request, ServerCallContext context)
