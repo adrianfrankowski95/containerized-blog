64814473d9a8edc31f8cebcb8556d7ce7465492b
AdrianFrankowski <adrianfrankowski95@gmail.com>
2022-06-22 22:51:00 +0200

    Implemented Grpc requests

--- src/Services/Discovery/Discovery.API/Grpc/DiscoveryService.cs
+++ src/Services/Discovery/Discovery.API/Grpc/DiscoveryService.cs
@@ -14,13 +14,32 @@ public class DiscoveryService : GrpcDiscoveryService.GrpcDiscoveryServiceBase
         _serviceRegistry = serviceRegistry ?? throw new ArgumentNullException(nameof(serviceRegistry));
     }
 
-    public override Task<GetUrlsOfServiceResponse> GetUrlsOfService(GetUrlsOfServiceRequest request, ServerCallContext context)
+    public override async Task<GetUrlsOfServiceResponse> GetUrlsOfService(GetUrlsOfServiceRequest request, ServerCallContext context)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrWhiteSpace(request.ServiceType))
+            throw new ArgumentNullException(nameof(request.ServiceType));
+
+        _logger.LogInformation("----- Handling Grpc get urls of service request for {ServiceType}", request.ServiceType);
+
+        var urls = await _serviceRegistry.GetUrlsOfServiceAsync(request.ServiceType).ConfigureAwait(false);
+
+        _logger.LogInformation("----- Successfully fetched following {ServiceType} URLs: {Urls}", request.ServiceType, string.Join("; ", urls));
+
+        return new GetUrlsOfServiceResponse { Urls = { urls } };
     }
 
-    public override Task<GetUrlsResponse> GetUrls(Empty request, ServerCallContext context)
+    public override async Task<GetUrlsResponse> GetUrls(Empty request, ServerCallContext context)
     {
-        throw new NotImplementedException();
+        _logger.LogInformation("----- Handling Grpc get urls request");
+
+        var servicesUrls = await _serviceRegistry.GetUrlsAsync().ConfigureAwait(false);
+
+        _logger.LogInformation("----- Successfully fetched following URLs: {ServiceUrls}",
+            servicesUrls.Select((serviceUrls) => $"{serviceUrls.Key}: {string.Join("; ", serviceUrls.Value)}"));
+
+        return new GetUrlsResponse
+        {
+            ServiceUrls = { servicesUrls.ToDictionary(k => k.Key, k => new UrlList { Urls = { k.Value } }) }
+        };
     }
 }
