51f1ce2c633ee0b18a1f26f44ecf0e513f2e00de
AdrianFrankowski <adrianfrankowski95@gmail.com>
2022-06-25 13:46:40 +0200

    Added routes config to gateway

--- src/Gateways/WebGateway.API/Services/IIdentityService.cs
+++
@@ -1,9 +0,0 @@
-using System.Security.Claims;
-
-namespace Blog.Gateways.WebGateway.API.Models;
-
-public interface IIdentityService
-{
-    public bool TryCreateUserIdentityFromAccessToken(string accessToken, out UserIdentity userIdentity);
-    public bool TryCreateUserIdentityFromClaimsPrincipal(ClaimsPrincipal claimsPrincipal, out UserIdentity userIdentity);
-}
