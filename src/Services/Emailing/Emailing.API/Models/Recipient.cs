c9baa5a8e762b1bebbcdd7cc24eaa4323c10a0a2
Adrian Frankowski <adrian.frankowski95@gmail.com>
2022-07-17 19:39:57 +0200

    Added Email DTOs

--- src/Services/Emailing/Emailing.API/Models/Recipient.cs
+++ src/Services/Emailing/Emailing.API/Models/Recipient.cs
@@ -1,3 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Blog.Services.Emailing.API.Models;
 
-public record Recipient(string EmailAddress, string Name);
+public record Recipient
+{
+    public string EmailAddress { get; init; }
+    public string Name { get; init; }
+    public Recipient(string emailAddress, string name)
+    {
+        if (string.IsNullOrWhiteSpace(emailAddress))
+            throw new ArgumentNullException(nameof(emailAddress));
+
+        if (!new EmailAddressAttribute().IsValid(emailAddress))
+            throw new FormatException(nameof(emailAddress));
+
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentNullException(nameof(name));
+
+        EmailAddress = emailAddress;
+        Name = name;
+    }
+}
