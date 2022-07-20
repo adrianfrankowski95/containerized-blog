fb84bb97ac929da66837d3b56612d0be0c973e9e
AdrianFrankowski <adrianfrankowski95@gmail.com>
2022-07-21 00:48:14 +0200

    Added read only collections

--- src/Gateways/WebGateway.API/Services/IInMemoryProxyConfigProvider.cs
+++ src/Gateways/WebGateway.API/Services/IInMemoryProxyConfigProvider.cs
@@ -7,9 +7,9 @@ public interface IInMemoryProxyConfigProvider : IProxyConfigProvider
 {
     public void GenerateRoutes(string serviceType, IEnumerable<(string incomingPath, string outgoingPath)> matchingPaths, ref List<RouteConfig> routes);
 
-    public Dictionary<string, DestinationConfig> GenerateDestinations(string serviceType, HashSet<ServiceInstanceInfo> instancesInfo);
+    public Dictionary<string, DestinationConfig> GenerateDestinations(string serviceType, IReadOnlySet<ServiceInstanceInfo> instancesInfo);
 
-    public void GenerateCluster(string clusterId, Dictionary<string, DestinationConfig> destinations, ref List<ClusterConfig> clusters);
+    public void GenerateCluster(string clusterId, IReadOnlyDictionary<string, DestinationConfig> destinations, ref List<ClusterConfig> clusters);
 
     public void Update(IReadOnlyList<RouteConfig> routes, IReadOnlyList<ClusterConfig> clusters);
 }
