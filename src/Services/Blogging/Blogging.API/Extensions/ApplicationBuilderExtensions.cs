b12587ecfb59c4f8e0d5edac0eed7a81fe43e462
AdrianFrankowski <adrianfrankowski95@gmail.com>
2022-09-27 21:02:51 +0200

    Added logger extensions

--- src/Services/Blogging/Blogging.API/Extensions/ApplicationBuilderExtensions.cs
+++ src/Services/Blogging/Blogging.API/Extensions/ApplicationBuilderExtensions.cs
@@ -2,7 +2,7 @@ using Blog.Services.Blogging.API.Middlewares;
 
 namespace Blog.Services.Blogging.API.Extensions;
 
-public static class ServiceCollectionExtensions
+public static class ApplicationBuilderExtensions
 {
     public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder builder)
      => builder.UseMiddleware<GlobalExceptionHandlerMiddleware>();
