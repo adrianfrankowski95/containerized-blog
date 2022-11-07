4098f8de0eccd9c444bdc62995485068c725c60d
AdrianFrankowski <adrianfrankowski95@gmail.com>
2022-11-07 23:30:56 +0100

    Removed redundant lines

--- src/Services/Identity/Identity.API/Pages/Error.cshtml.cs
+++ src/Services/Identity/Identity.API/Pages/Error.cshtml.cs
@@ -12,9 +12,7 @@ public class ErrorModel : PageModel
 
     public string RequestId { get; set; }
 
-
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
 
     public void OnGet()
     {
