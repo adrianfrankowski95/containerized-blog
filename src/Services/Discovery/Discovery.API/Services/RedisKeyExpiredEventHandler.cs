09772d4d29c6f9a60d8de708f4345598314d439a
adrianfrankowski95 <adrianfrankowski95@gmail.com>
2022-11-25 10:20:25 +0100

    Revert back to IHostedService implementation

--- src/Services/Discovery/Discovery.API/Services/RedisKeyExpiredEventHandler.cs
+++ src/Services/Discovery/Discovery.API/Services/RedisKeyExpiredEventHandler.cs
@@ -5,39 +5,27 @@ using StackExchange.Redis;
 
 namespace Blog.Services.Discovery.API.Services;
 
-public class RedisKeyExpiredEventHandler : BackgroundService
+public class RedisKeyExpiredEventHandler : IHostedService
 {
     private readonly IBus _bus;
     private readonly IConnectionMultiplexer _redis;
-    private readonly IHostApplicationLifetime _lifetime;
     private readonly ILogger<RedisKeyExpiredEventHandler> _logger;
 
     public RedisKeyExpiredEventHandler(
         IBus bus,
         IConnectionMultiplexer redis,
-        IHostApplicationLifetime lifetime,
         ILogger<RedisKeyExpiredEventHandler> logger)
     {
         _bus = bus ?? throw new ArgumentNullException(nameof(redis));
         _redis = redis ?? throw new ArgumentNullException(nameof(redis));
-        _lifetime = lifetime ?? throw new ArgumentNullException(nameof(lifetime));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        await WaitForStartupOrCancellationAsync(_lifetime, cancellationToken, out CancellationTokenRegistration tokenRegistration)
-                    .ConfigureAwait(false);
+        cancellationToken.ThrowIfCancellationRequested();
 
-        await tokenRegistration.DisposeAsync().ConfigureAwait(false);
-
-        if (cancellationToken.IsCancellationRequested)
-        {
-            _logger.LogWarning("----- Cancellation requested for discovery service.");
-            return;
-        }
-        Console.WriteLine("Subscribing to redis");
-        await _redis.GetSubscriber(_redis.GetServer(_redis.GetEndPoints().Single())).SubscribeAsync("__keyevent@0__:expired", async (channel, message) =>
+        return _redis.GetSubscriber(_redis.GetServer(_redis.GetEndPoints().Single())).SubscribeAsync("__keyevent@0__:expired", async (channel, message) =>
         {
             _logger.LogInformation("----- Received key expired event from Redis, channel: {Channel}, message: {Message}.", channel, message);
 
@@ -51,22 +39,9 @@ public class RedisKeyExpiredEventHandler : BackgroundService
         });
     }
 
-    public override async Task StopAsync(CancellationToken cancellationToken)
+    public async Task StopAsync(CancellationToken cancellationToken)
     {
         await _redis.GetSubscriber(_redis.GetServer(_redis.GetEndPoints().Single())).UnsubscribeAsync("__keyevent@0__:expired").ConfigureAwait(false);
         cancellationToken.ThrowIfCancellationRequested();
-    }
-
-    private static Task WaitForStartupOrCancellationAsync(
-        IHostApplicationLifetime lifetime,
-        CancellationToken stoppingToken,
-        out CancellationTokenRegistration registration)
-    {
-        var appStartedOrCancelled = new TaskCompletionSource();
-        var startedOrCancelledToken = CancellationTokenSource.CreateLinkedTokenSource(lifetime.ApplicationStarted, stoppingToken).Token;
-
-        registration = startedOrCancelledToken.Register(() => appStartedOrCancelled.SetResult());
-
-        return appStartedOrCancelled.Task;
     }
 }
